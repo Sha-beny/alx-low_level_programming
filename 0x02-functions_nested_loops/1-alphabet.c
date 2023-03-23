e998e15863df93c9bac70325051e04222fe93cc8
sha-beny <sharonben4real@gmail.com>
2023-03-23 06:58:25 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,15 @@
 #include "main.h"
+
 /**
  * print_alphabet - print all alphabet in lowercase
- *
- * Return: Always 0(success)
  */
+
 void print_alphabet(void)
 {
-	int i;
+	char letter;
 
-	for (i = 97; i <= 122; i++)
-	{
+	for (letter = 'a'; letter <= 'z'; letter++)
 		_putchar(letter);
-	}
+
 	_putchar('\n');
 }
